728ac206079c52f00b5863f6ac04c7352987db94
code-wave <codewave080@gmail.com>
2025-03-16 02:06:11 +0900

    feat: 홈페이지 레이아웃 재설계 및 스크롤 기반 애니메이션 추가
    홈페이지의 새로운 디자인과 구조 및 스타일을 적용했으며 사용자 경험(UX)을 향상시키기 위해 React 상태를 활용한 스크롤 기반 애니메이션을 도입하였으며 배너, 지표, "Hire Me" 버튼과 같은 새로운 UI 컴포넌트를 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,93 @@
-import Image from 'next/image'
+'use client'
+
+import { useState } from 'react'
 
 export default function Home() {
-    return (
-        <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-            <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-                <Image
-                    className="dark:invert"
-                    src="/next.svg"
-                    alt="Next.js logo"
-                    width={180}
-                    height={38}
-                    priority
-                />
-                <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-                    <li className="mb-2 tracking-[-.01em]">
-                        Get started by editing{' '}
-                        <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-                            src/app/page.tsx
-                        </code>
-                        .
-                    </li>
-                    <li className="tracking-[-.01em]">
-                        Save and see your changes instantly.
-                    </li>
-                </ol>
+    const [translateY, setTranslateY] = useState<number>(-100)
 
-                <div className="flex gap-4 items-center flex-col sm:flex-row">
-                    <a
-                        className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-                        href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        <Image
-                            className="dark:invert"
-                            src="/vercel.svg"
-                            alt="Vercel logomark"
-                            width={20}
-                            height={20}
-                        />
-                        Deploy now
-                    </a>
-                    <a
-                        className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-                        href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        Read our docs
-                    </a>
+    return (
+        <div
+            className="h-screen overflow-y-auto bg-gray-100"
+            onScroll={(e) => {
+                const { scrollTop, scrollHeight, clientHeight } =
+                    e.currentTarget
+                const scrollPercentage =
+                    (scrollTop / (scrollHeight - clientHeight)) * 100
+
+                // translateY 값 업데이트 (예: -100%에서 0%로 이동)
+                setTranslateY(-100 + scrollPercentage)
+            }}
+        >
+            <div className="grid grid-rows-[0px_1fr_65px] bg-[#212121]">
+                <div className="sticky top-0 z-[4]">
+                    <button className="group size-12 absolute top-5 right-5 md:right-10">
+                        <span className="inline-block w-3/5 h-0.5 rounded-full absolute left-1/2 -translate-x-1/2 top-1/2 duration-300 -translate-y-[5px] md:group-hover:rotate-12 bg-[#DEDEDE]"></span>
+                        <span className="inline-block w-3/5 h-0.5 rounded-full absolute left-1/2 -translate-x-1/2 top-1/2 duration-300 translate-y-[5px] md:group-hover:-rotate-12 bg-[#DEDEDE]"></span>
+                    </button>
                 </div>
-            </main>
-            <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-                <a
-                    className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-                    href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    <Image
-                        aria-hidden
-                        src="/file.svg"
-                        alt="File icon"
-                        width={16}
-                        height={16}
-                    />
-                    Learn
-                </a>
-                <a
-                    className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-                    href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    <Image
-                        aria-hidden
-                        src="/window.svg"
-                        alt="Window icon"
-                        width={16}
-                        height={16}
-                    />
-                    Examples
-                </a>
-                <a
-                    className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-                    href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    <Image
-                        aria-hidden
-                        src="/globe.svg"
-                        alt="Globe icon"
-                        width={16}
-                        height={16}
-                    />
-                    Go to nextjs.org →
-                </a>
-            </footer>
+                <div className="page-">
+                    <section className="relative overflow-hidden" id="banner">
+                        <div className="container h-[100svh] min-h-[530px] flex flex-col justify-center ml-20 gap-5">
+                            <h1 className="flex flex-col gap-1 text-7xl font-bold font-sans">
+                                <span className="text-pink-500">FRONTEND</span>
+                                <span className="text-[#DEDEDE]">
+                                    DEVELOPER
+                                </span>
+                            </h1>
+                            <p className="text-[#DEDEDE] opacity-70">
+                                <span className="text-white font-bold opacity-100">
+                                    &quot;코드도 UX도 녹이 슨다.&quot;
+                                </span>{' '}
+                                지속 가능한 개발을 고민하는 프론트엔드
+                                개발자입니다.
+                                <br />
+                                새로운 기술을 배우고 적용하는 과정에서 끊임없이
+                                성장하는 개발자를 목표로 합니다.
+                            </p>
+                            <a
+                                href=""
+                                className="group w-45 h-12 px-8 inline-flex justify-center items-center gap-2 text-lg uppercase font-anton tracking-widest outline-none transition-colors relative overflow-hidden bg-pink-500 text-white font-bold hover:bg-primary-hover hover:text-pink-500 mt-4 banner-button slide-up-and-fade"
+                            >
+                                <span className="absolute top-[200%] left-0 right-0 h-full bg-white rounded-[50%] group-hover:top-0 transition-all duration-500 scale-150"></span>
+                                <span className="z-[1]">HIRE ME</span>
+                            </a>
+                        </div>
+                        <div className="md:absolute bottom-[10%] right-[4%] flex md:flex-col gap-4 md:gap-8 text-center md:text-right">
+                            <div className="slide-up-and-fade flex flex-col gap-1">
+                                <h5 className="text-3xl sm:text-3.5xl font-anton font-bold text-pink-500">
+                                    3+
+                                </h5>
+                                <p className="text-[#DEDEDE] text-sm opacity-70">
+                                    Years of Experience
+                                </p>
+                            </div>
+                            <div className="slide-up-and-fade flex flex-col gap-1">
+                                <h5 className="text-3xl sm:text-3.5xl font-anton font-bold text-pink-500">
+                                    6+
+                                </h5>
+                                <p className="text-[#DEDEDE] text-sm opacity-70">
+                                    Completed Projects
+                                </p>
+                            </div>
+                            <div className="slide-up-and-fade flex flex-col gap-1">
+                                <h5 className="text-3xl sm:text-3.5xl font-anton font-bold text-pink-500">
+                                    5.8K+
+                                </h5>
+                                <p className="text-[#DEDEDE] text-sm opacity-70">
+                                    Lines of Code Written
+                                </p>
+                            </div>
+                        </div>
+                    </section>
+                </div>
+                <div className="footer"></div>
+            </div>
+            <div className="fixed top-[50svh] right-[2%] -translate-y-1/2 w-1.5 h-[100px] rounded-full bg-[#313131] overflow-hidden">
+                <div
+                    className="w-full bg-pink-500 rounded-full h-full"
+                    style={{ transform: `translateY(${translateY}%)` }}
+                ></div>
+            </div>
         </div>
     )
 }
