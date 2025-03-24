4462c3bc40b53c76f275e244f894ea9ab2d93b15
code-wave <codewave080@gmail.com>
2025-03-24 23:14:24 +0900

    feat: SVGR을 사용한 SVG 지원 추가 및 빌드 종속성 업데이트
    SVG를 React 컴포넌트로 가져올 수 있도록 SVGR 설정을 추가하고 관련 타입 정의를 추가했습니다. 새로운 의존성인 `next-svgr`와 `webpack`을 포함하여 종속성을 업데이트했으며, `dev` 스크립트에서 사용하지 않는 Turbopack을 제거했습니다. 이러한 변경 사항을 지원하기 위해 설정 파일도 수정했습니다.

---
+++ next.config.js
@@ -0,0 +1,25 @@
+// next.config.js
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    webpack(config) {
+        // SVG 파일은 기본 file-loader에서 제외
+        const fileLoaderRule = config.module.rules.find((rule) =>
+            rule.test?.test?.('.svg')
+        );
+        if (fileLoaderRule) {
+            fileLoaderRule.exclude = /\.svg$/i;
+        }
+
+        // SVGR 로더 추가
+        config.module.rules.push({
+            test: /\.svg$/i,
+            issuer: /\.[jt]sx?$/,
+            use: ['@svgr/webpack'],
+        });
+
+        return config;
+    },
+};
+
+module.exports = nextConfig;
