3d53dc6b960ec25921c38ccc58ee9d219709a0cb
code-wave <codewave080@gmail.com>
2025-03-31 21:12:59 +0900

    chore: MyStack 컴포넌트 리팩토링으로 코드 일관성 개선

--- src/app/components/MyStack.tsx
+++ src/app/components/MyStack.tsx
@@ -1,26 +1,26 @@
-import JavascriptIcon from '@/icons/javascript.svg';
-import TypescriptIcon from '@/icons/typescript.svg';
-import VueIcon from '@/icons/vuedotjs.svg';
-import ReactIcon from '@/icons/react.svg';
-import NextIcon from '@/icons/nextdotjs.svg';
-import TailwindIcon from '@/icons/tailwindcss.svg';
-import FramerIcon from '@/icons/framer.svg';
-import SassIcon from '@/icons/sass.svg';
-import StyledComponentsIcon from '@/icons/styledcomponents.svg';
-import GitIcon from '@/icons/git.svg';
-import GithubIcon from '@/icons/github.svg';
-import VercelIcon from '@/icons/vercel.svg';
-import FirebaseIcon from '@/icons/firebase.svg';
-import EslintIcon from '@/icons/eslint.svg';
-import PrettierIcon from '@/icons/prettier.svg';
-import StorybookIcon from '@/icons/storybook.svg';
-import IntellijIcon from '@/icons/intellijidea.svg';
-import EchartsIcon from '@/icons/apacheecharts.svg';
-import SlackIcon from '@/icons/slack.svg';
-import NotionIcon from '@/icons/notion.svg';
-import FigmaIcon from '@/icons/figma.svg';
-import GiraIcon from '@/icons/jira.svg';
-import React from 'react';
+import JavascriptIcon from '@/icons/javascript.svg'
+import TypescriptIcon from '@/icons/typescript.svg'
+import VueIcon from '@/icons/vuedotjs.svg'
+import ReactIcon from '@/icons/react.svg'
+import NextIcon from '@/icons/nextdotjs.svg'
+import TailwindIcon from '@/icons/tailwindcss.svg'
+import FramerIcon from '@/icons/framer.svg'
+import SassIcon from '@/icons/sass.svg'
+import StyledComponentsIcon from '@/icons/styledcomponents.svg'
+import GitIcon from '@/icons/git.svg'
+import GithubIcon from '@/icons/github.svg'
+import VercelIcon from '@/icons/vercel.svg'
+import FirebaseIcon from '@/icons/firebase.svg'
+import EslintIcon from '@/icons/eslint.svg'
+import PrettierIcon from '@/icons/prettier.svg'
+import StorybookIcon from '@/icons/storybook.svg'
+import IntellijIcon from '@/icons/intellijidea.svg'
+import EchartsIcon from '@/icons/apacheecharts.svg'
+import SlackIcon from '@/icons/slack.svg'
+import NotionIcon from '@/icons/notion.svg'
+import FigmaIcon from '@/icons/figma.svg'
+import GiraIcon from '@/icons/jira.svg'
+import React from 'react'
 
 export default function MyStack() {
     const iconMap: Record<string, React.FC<React.SVGProps<SVGSVGElement>>> = {
@@ -46,30 +46,38 @@ export default function MyStack() {
         notion: NotionIcon,
         figma: FigmaIcon,
         jira: GiraIcon,
-    };
+    }
 
     const stacks = [
         {
             label: 'Language & Framework',
             value: 'LanguageAndFramework',
             items: [
-                {label: 'Javascript', value: 'javascript', color: '#F7DF1E'},
-                {label: 'TypeScript', value: 'typescript', color: '#3178C6'},
-                {label: 'Vue.js', value: 'vuedotjs', color: '#4FC08D'},
-                {label: 'React', value: 'react', color: '#61DAFB'},
-                {label: 'Next.js', value: 'nextdotjs', color: '#000000'}
-            ]
+                { label: 'Javascript', value: 'javascript', color: '#F7DF1E' },
+                { label: 'TypeScript', value: 'typescript', color: '#3178C6' },
+                { label: 'Vue.js', value: 'vuedotjs', color: '#4FC08D' },
+                { label: 'React', value: 'react', color: '#61DAFB' },
+                { label: 'Next.js', value: 'nextdotjs', color: '#000000' },
+            ],
         },
         {
             label: 'Styling & Animation',
             value: 'StylingAndAnimation',
             items: [
-                { label: 'Tailwind CSS', value: 'tailwindcss', color: '#06B6D4' },
+                {
+                    label: 'Tailwind CSS',
+                    value: 'tailwindcss',
+                    color: '#06B6D4',
+                },
                 { label: 'SASS', value: 'sass', color: '#CC6699' },
                 { label: 'Emotion', value: 'emotion', color: '#CC6699' },
-                { label: 'styled-components', value: 'styledComponents', color: '#DB7093' },
+                {
+                    label: 'styled-components',
+                    value: 'styledComponents',
+                    color: '#DB7093',
+                },
                 { label: 'Framer Motion', value: 'framer', color: '#0055FF' },
-            ]
+            ],
         },
         {
             label: 'Deployment & Development Tools',
@@ -83,14 +91,12 @@ export default function MyStack() {
                 { label: 'Prettier', value: 'prettier', color: '#F7B93E' },
                 { label: 'Storybook', value: 'storybook', color: '#FF4785' },
                 { label: 'intellij', value: 'intellij', color: '#000000' },
-            ]
+            ],
         },
         {
             label: 'Data & Visualization',
             value: 'DataAndVisualization',
-            items: [
-                { label: 'ECharts', value: 'echarts', color: '#AA344D' }
-            ]
+            items: [{ label: 'ECharts', value: 'echarts', color: '#AA344D' }],
         },
         {
             label: 'Collaboration & Communication',
@@ -99,10 +105,10 @@ export default function MyStack() {
                 { label: 'Slack', value: 'slack', color: '#4A154B' },
                 { label: 'Notion', value: 'notion', color: '#000000' },
                 { label: 'Figma', value: 'figma', color: '#F24E1E' },
-                { label: 'Jira', value: 'jira', color: '#0052CC' }
-            ]
-        }
-    ];
+                { label: 'Jira', value: 'jira', color: '#0052CC' },
+            ],
+        },
+    ]
 
     return (
         <div className="container">
@@ -111,7 +117,7 @@ export default function MyStack() {
             </div>
             <div className="space-y-20">
                 <div className="grid sm:grid-cols-12 gap-8">
-                    {stacks.map(({label, items}, index) => (
+                    {stacks.map(({ label, items }, index) => (
                         <React.Fragment key={index}>
                             <div className="sm:col-span-5">
                                 <p className="slide-up text-3xl font-anton leading-none text-muted-foreground uppercase font-bold text-[#B5B5B5]">
@@ -120,7 +126,7 @@ export default function MyStack() {
                             </div>
                             <div className="sm:col-span-7 flex gap-x-11 gap-y-9 flex-wrap">
                                 {items.map((item, index) => {
-                                    const Icon = iconMap[item.value];
+                                    const Icon = iconMap[item.value]
                                     return (
                                         <div
                                             className="slide-up flex gap-3.5 items-center leading-none"
@@ -130,15 +136,17 @@ export default function MyStack() {
                                                 {Icon && (
                                                     <Icon
                                                         className="w-full h-full"
-                                                        style={{ fill: item.color }}
+                                                        style={{
+                                                            fill: item.color,
+                                                        }}
                                                     />
                                                 )}
                                             </div>
                                             <span className="text-1xl capitalize text-[#DEDEDE]">
-                                        {item.label}
-                                    </span>
+                                                {item.label}
+                                            </span>
                                         </div>
-                                    );
+                                    )
                                 })}
                             </div>
                         </React.Fragment>
@@ -146,5 +154,5 @@ export default function MyStack() {
                 </div>
             </div>
         </div>
-    );
+    )
 }
