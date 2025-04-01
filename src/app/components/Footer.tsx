d77fae868033ad1f072b2cfed69845e816130faf
code-wave <codewave080@gmail.com>
2025-04-01 23:09:18 +0900

    feat: Footer 컴포넌트에 연락처 정보 추가

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,3 +1,19 @@
 export default function Footer() {
-    return <div className="footer h-[2000px]"></div>
+    return (
+        <footer className="text-center pb-5 text-[#DEDEDE]">
+            <div className="container">
+                <p className="text-lg">
+                    프로젝트 협업 또는 채용 문의를 기다리고 있습니다
+                </p>
+                <p className="text-lg">언제든지 메일로 연락 주세요</p>
+                <a
+                    href="mailto:codewave080@gmail.com"
+                    className="text-3xl sm:text-4xl font-anton inline-block mt-5 mb-10 hover:underline"
+                >
+                    codewave080@gmail.com
+                </a>
+                {/*<p>© 2025 codewave. All rights reserved.</p>*/}
+            </div>
+        </footer>
+    )
 }
