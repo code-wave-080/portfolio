ac347b89bb5c236e8ea06fa76c8c62211a9f9abb
code-wave <codewave080@gmail.com>
2025-04-01 23:11:50 +0900

    refactor: MyStack 및 Banner 컴포넌트의 레이아웃과 구조 리팩토링
    HTML 구조를 개선하여 더 나은 시맨틱 구성과 유지보수성을 확보했습니다. `<div>` 컨테이너를 `<section>` 요소로 교체했으며, 기존 모습을 유지하기 위해 스타일을 조정했습니다.

--- src/app/components/MyStack.tsx
+++ src/app/components/MyStack.tsx
@@ -111,48 +111,50 @@ export default function MyStack() {
     ]
 
     return (
-        <div className="container">
-            <div className="flex items-center gap-4 mb-10 text-[#DEDEDE]">
-                <h2 className="text-xl uppercase leading-none">MY STACK</h2>
-            </div>
-            <div className="space-y-20">
-                <div className="grid sm:grid-cols-12 gap-8">
-                    {stacks.map(({ label, items }, index) => (
-                        <React.Fragment key={index}>
-                            <div className="sm:col-span-5">
-                                <p className="slide-up text-3xl font-anton leading-none text-muted-foreground uppercase font-bold text-[#B5B5B5]">
-                                    {label}
-                                </p>
-                            </div>
-                            <div className="sm:col-span-7 flex gap-x-11 gap-y-9 flex-wrap">
-                                {items.map((item, index) => {
-                                    const Icon = iconMap[item.value]
-                                    return (
-                                        <div
-                                            className="slide-up flex gap-3.5 items-center leading-none"
-                                            key={index}
-                                        >
-                                            <div className="rounded-full w-10 h-10 bg-[#313131] overflow-hidden flex items-center justify-center">
-                                                {Icon && (
-                                                    <Icon
-                                                        className="w-full h-full"
-                                                        style={{
-                                                            fill: item.color,
-                                                        }}
-                                                    />
-                                                )}
+        <section className="pb-section" id="my-stack">
+            <div className="container">
+                <div className="flex items-center gap-4 mb-10 text-[#DEDEDE]">
+                    <h2 className="text-xl uppercase leading-none">MY STACK</h2>
+                </div>
+                <div className="space-y-20">
+                    <div className="grid sm:grid-cols-12 gap-8">
+                        {stacks.map(({ label, items }, index) => (
+                            <React.Fragment key={index}>
+                                <div className="sm:col-span-5">
+                                    <p className="slide-up text-3xl font-anton leading-none text-muted-foreground uppercase font-bold text-[#B5B5B5]">
+                                        {label}
+                                    </p>
+                                </div>
+                                <div className="sm:col-span-7 flex gap-x-11 gap-y-9 flex-wrap">
+                                    {items.map((item, index) => {
+                                        const Icon = iconMap[item.value]
+                                        return (
+                                            <div
+                                                className="slide-up flex gap-3.5 items-center leading-none"
+                                                key={index}
+                                            >
+                                                <div className="rounded-full w-10 h-10 bg-[#313131] overflow-hidden flex items-center justify-center">
+                                                    {Icon && (
+                                                        <Icon
+                                                            className="w-full h-full"
+                                                            style={{
+                                                                fill: item.color,
+                                                            }}
+                                                        />
+                                                    )}
+                                                </div>
+                                                <span className="text-1xl capitalize text-[#DEDEDE]">
+                                                    {item.label}
+                                                </span>
                                             </div>
-                                            <span className="text-1xl capitalize text-[#DEDEDE]">
-                                                {item.label}
-                                            </span>
-                                        </div>
-                                    )
-                                })}
-                            </div>
-                        </React.Fragment>
-                    ))}
+                                        )
+                                    })}
+                                </div>
+                            </React.Fragment>
+                        ))}
+                    </div>
                 </div>
             </div>
-        </div>
+        </section>
     )
 }
