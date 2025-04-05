f7731ca8dba9c9091c651eed14694ba3bbdc3845
code-wave <codewave080@gmail.com>
2025-04-05 23:58:26 +0900

    refactor: 섹션 ID 변경 및 MyProjects 레이아웃을 위한 컨테이너 div 추가 작업 중
    섹션 ID를 "my-project"에서 "my-projects"로 변경하여 명명 규칙을 일관성 있게 수정했습니다. 향후 프로젝트 콘텐츠의 더 나은 정렬과 레이아웃을 위해 새로운 flex 컨테이너 div를 추가했습니다.

--- src/app/components/MyProjects.tsx
+++ src/app/components/MyProjects.tsx
@@ -2,11 +2,12 @@ import React from "react";
 
 export default function MyProjects() {
     return (
-        <section className="pb-section text-[#DEDEDE]" id="my-project">
+        <section className="pb-section text-[#DEDEDE]" id="my-projects">
             <div className="container">
                 <div className="flex items-center gap-4 mb-10 text-[#DEDEDE]">
                     <h2 className="text-xl uppercase leading-none">MY PROJECTS</h2>
                 </div>
+                <div className="flex flex-wrap row-gap-40"></div>
             </div>
         </section>
     )
