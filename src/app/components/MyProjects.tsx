bf35aa23bbe960ca443576d9d95f624ac6fb0254
code-wave <codewave080@gmail.com>
2025-04-06 23:07:04 +0900

    feat: 프로젝트 카드에 hover 상태 및 동적 스타일 추가
    프로젝트 카드에 새로운 `isHovered` 상태를 활용하여 hover 효과를 추가했습니다. 동적 클래스 처리를 포함하도록 컴포넌트 구조를 업데이트하고, 더 나은 인터랙티브한 비주얼을 위해 샘플 콘텐츠를 포함했습니다.

--- src/app/components/MyProjects.tsx
+++ src/app/components/MyProjects.tsx
@@ -1,13 +1,40 @@
-import React from "react";
+import React, { useState } from 'react'
 
 export default function MyProjects() {
+    const [isHovered, setIsHovered] = useState(false)
+
     return (
         <section className="pb-section text-[#DEDEDE]" id="my-projects">
             <div className="container">
                 <div className="flex items-center gap-4 mb-10 text-[#DEDEDE]">
-                    <h2 className="text-xl uppercase leading-none">MY PROJECTS</h2>
+                    <h2 className="text-xl uppercase leading-none">
+                        MY PROJECTS
+                    </h2>
                 </div>
-                <div className="flex flex-wrap row-gap-40"></div>
+                <div className="pj-img-con flex flex-wrap gap-[40px]">
+                    <div
+                        onMouseEnter={() => setIsHovered(true)}
+                        onMouseLeave={() => setIsHovered(false)}
+                        className={`pj-img-box
+                         ${isHovered ? 'active' : ''}
+                          border-2 rounded-[20px] w-[calc(50%-20px)] h-[500px]`}
+                    >
+                        <div className="bg-wrap">
+                            <img src="/images/project-1.png" alt="project-1" />
+                        </div>
+                        <div className="txt-item">
+                            <p>BLOG</p>
+                            <ul className="hover-list-txt">
+                                <li>React</li>
+                                <li>Sass</li>
+                                <li>Firebase</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div className="pj-img-box border-2 rounded-[20px] w-[calc(50%-20px)] h-[500px]"></div>
+                    <div className="pj-img-box border-2 rounded-[20px] w-[calc(50%-20px)] h-[500px]"></div>
+                    <div className="pj-img-box border-2 rounded-[20px] w-[calc(50%-20px)] h-[500px]"></div>
+                </div>
             </div>
         </section>
     )
