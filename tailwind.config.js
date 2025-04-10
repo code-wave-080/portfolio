cd41570a2c64142c6d352c58b88393d7bd671c33
code-wave <codewave080@gmail.com>
2025-04-10 23:19:03 +0900

    feat: Tailwind 및 CSS에 새로운 slideWrapperUp 애니메이션 추가
    Tailwind 구성 및 전역 CSS에 "slideWrapperUp" 애니메이션을 도입했습니다. 이 애니메이션은 요소를 200vh 위로 이동시키며 ease-in-out 효과를 적용합니다.

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,11 +5,16 @@ module.exports = {
         extend: {
             animation: {
                 twinkle: 'twinkle 1.5s infinite alternate',
+                'slide-wrapper-up': 'slideWrapperUp 1.5s ease-in-out forwards',
             },
             keyframes: {
                 twinkle: {
                     '0%': { opacity: '0.2', transform: 'translateY(0px)' },
                     '100%': { opacity: '1', transform: 'translateY(-2px)' },
+                },
+                slideWrapperUp: {
+                    '0%': { transform: 'translateY(0%)' },
+                    '100%': { transform: 'translateY(-200vh)' },
                 },
             },
         },
