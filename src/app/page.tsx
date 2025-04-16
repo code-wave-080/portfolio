38a7bf6204f2d739cc032d275a4a51a7a0031029
code-wave <codewave080@gmail.com>
2025-04-16 23:48:04 +0900

    refactor: Home 페이지 구조 단순화 및 별(stars) 제거
    별 생성 로직, `useEffect`, 그리고 `Sidebar`와 `Footer` 컴포넌트를 제거했습니다. 페이지 렌더링을 간소화하기 위해 스크롤 가능한 컨테이너를 주석 처리했습니다. 유지보수성을 향상시키고 불필요한 복잡성을 줄이는 데 초점을 맞췄습니다.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,17 @@
 'use client'
-
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import ScrollProgress from '@/app/components/ScrollProgress'
-import Sidebar from '@/app/components/Sidebar'
 import Banner from '@/app/components/Banner'
 import AboutMe from '@/app/components/AboutMe'
 import MyStack from '@/app/components/MyStack'
 import MyExperience from '@/app/components/MyExperience'
 import MyProjects from '@/app/components/MyProjects'
-import Footer from '@/app/components/Footer'
 
 export default function Home() {
     const [translateY, setTranslateY] = useState<number>(-100)
-    const [stars, setStars] = useState<
-        { top: string; left: string; size: string; animate: boolean }[]
-    >([])
-
-    useEffect(() => {
-        const generatedStars = Array.from({ length: 50 }, () => ({
-            top: `${Math.random() * 100}%`,
-            left: `${Math.random() * 100}%`,
-            size: `${Math.random() * 3}px`,
-            animate: Math.random() > 0.5, // 50% 확률로 깜빡이는 애니메이션 적용
-        }))
-        setStars(generatedStars)
-    }, [])
 
     return (
-        <div
+        /*<div
             className="relative w-full h-screen overflow-x-hidden overflow-y-auto hide-scrollbar bg-[#212121]"
             onScroll={(e) => {
                 const { scrollTop, scrollHeight, clientHeight } =
@@ -39,28 +23,21 @@ export default function Home() {
                 setTranslateY(-100 + scrollPercentage)
             }}
         >
-            {stars.map((star, i) => (
-                <div
-                    key={i}
-                    className={`fixed bg-white rounded-full ${star.animate ? 'twinkle-move' : ''}`}
-                    style={{
-                        width: star.size,
-                        height: star.size,
-                        top: star.top,
-                        left: star.left,
-                    }}
-                />
-            ))}
-            <Sidebar />
             <div className="page-">
                 <Banner />
                 <AboutMe />
                 <MyStack />
                 <MyExperience />
                 <MyProjects />
-                <Footer />
             </div>
             <ScrollProgress value={translateY} />
+        </div>*/
+        <div className="page-">
+            <Banner />
+            <AboutMe />
+            <MyStack />
+            <MyExperience />
+            <MyProjects />
         </div>
     )
 }
