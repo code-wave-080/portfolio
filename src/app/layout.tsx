da4c1968c4511d3aa943ed3e7108b42ba43f29ae
code-wave <codewave080@gmail.com>
2025-04-16 23:47:25 +0900

    refactor: 레이아웃에 Sidebar와 Footer 컴포넌트 추가
    LayoutClient를 제거하고 Sidebar, Footer, main 콘텐츠 영역을 포함하는 구조화된 레이아웃으로 변경했습니다. 이를 통해 앱 레이아웃의 명확성과 유연성이 향상되었습니다.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,9 @@
 import type { Metadata } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
 import './globals.css'
-import LayoutClient from './layout-client'
+import Sidebar from "@/app/components/Sidebar";
+import Footer from "@/app/components/Footer";
+import React from "react";
 
 const geistSans = Geist({
     variable: '--font-geist-sans',
@@ -26,7 +28,11 @@ export default function RootLayout({
             <body
                 className={`${geistSans.variable} ${geistMono.variable} antialiased`}
             >
-                <LayoutClient>{children}</LayoutClient>
+                <div className="flex min-h-screen flex-col">
+                    <Sidebar />
+                    <main className="flex-1">{children}</main>
+                    <Footer />
+                </div>
             </body>
         </html>
     )
