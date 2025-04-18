8a60a73f6b52eb04dccff4903c8a186320337d6d
code-wave <codewave080@gmail.com>
2025-04-18 22:45:58 +0900

    refactor: 사용되지 않는 코드를 제거하여 Home 컴포넌트 단순화
    사용되지 않는 `useState`와 `ScrollProgress` import를 제거하고, 주석 처리된 불필요한 스크롤 처리 로직도 삭제했습니다. 이번 정리 작업으로 코드 가독성을 향상시키고 Home 컴포넌트에 필요한 기능만 유지했습니다.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 'use client'
-import { useState } from 'react'
-import ScrollProgress from '@/app/components/ScrollProgress'
+
 import Banner from '@/app/components/Banner'
 import AboutMe from '@/app/components/AboutMe'
 import MyStack from '@/app/components/MyStack'
@@ -8,30 +7,7 @@ import MyExperience from '@/app/components/MyExperience'
 import MyProjects from '@/app/components/MyProjects'
 
 export default function Home() {
-    const [translateY, setTranslateY] = useState<number>(-100)
-
     return (
-        /*<div
-            className="relative w-full h-screen overflow-x-hidden overflow-y-auto hide-scrollbar bg-[#212121]"
-            onScroll={(e) => {
-                const { scrollTop, scrollHeight, clientHeight } =
-                    e.currentTarget
-                const scrollPercentage =
-                    (scrollTop / (scrollHeight - clientHeight)) * 100
-
-                // translateY 값 업데이트 (예: -100%에서 0%로 이동)
-                setTranslateY(-100 + scrollPercentage)
-            }}
-        >
-            <div className="page-">
-                <Banner />
-                <AboutMe />
-                <MyStack />
-                <MyExperience />
-                <MyProjects />
-            </div>
-            <ScrollProgress value={translateY} />
-        </div>*/
         <div className="page-">
             <Banner />
             <AboutMe />
