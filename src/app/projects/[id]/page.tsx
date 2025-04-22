d8479df34325455bc0987759e3497114cdd2107f
code-wave <codewave080@gmail.com>
2025-04-22 23:32:27 +0900

    refactor: 로직 단순화 및 API 호출 제거를 위한 ProjectPage 리팩토링
    ProjectPage에서 fetch 요청과 불필요한 데이터 처리를 제거했습니다.

--- src/app/projects/[id]/page.tsx
+++ src/app/projects/[id]/page.tsx
@@ -5,12 +5,7 @@ interface ProjectPageProps {
 }
 
 export default async function ProjectPage({ params }: ProjectPageProps) {
-    const {id} = params;
-    const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/meta/${id}`, {
-        cache: 'no-store'
-    })
+    const { id } = params
 
-    const data = await response.json();
-
-    return <div className="text-white">프로젝트 상세 페이지 - title: {data.title} - description: {data.description}</div>
+    return <div>프로젝트 상세 페이지 - ID: {id}</div>
 }
