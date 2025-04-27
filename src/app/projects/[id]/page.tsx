09dd65cc2f075281f984b358ee5a79eb4e78d5ba
code-wave <codewave080@gmail.com>
2025-04-27 22:26:03 +0900

    refactor: 구조 개선 및 애니메이션 추가를 위한 프로젝트 페이지 리팩토링
    프로젝트 페이지를 클라이언트 사이드 렌더링을 사용하도록 재구성하고, 향상된 레이아웃과 애니메이션을 도입했습니다. 더 역동적인 사용자 경험을 위해 TransitionLink를 이용한 네비게이션을 추가하고, 새로운 컴포넌트와 스타일로 UI를 업데이트했습니다.

--- src/app/projects/[id]/page.tsx
+++ src/app/projects/[id]/page.tsx
@@ -1,11 +1,79 @@
+'use client'
+
+import TransitionLink from '@/components/TransitionLink'
+import { use, useRef } from 'react'
+import { ChevronLeft } from 'lucide-react'
+
 interface ProjectPageProps {
-    params: {
-        id: string
-    }
+    id: string
 }
 
-export default async function ProjectPage({ params }: ProjectPageProps) {
-    const { id } = params
+export default function ProjectPage({
+    params,
+}: {
+    params: Promise<ProjectPageProps>
+}) {
+    const { id } = use(params) as ProjectPageProps
+    const containerRef = useRef<HTMLDivElement>(null)
 
-    return <div>프로젝트 상세 페이지 - ID: {id}</div>
+    return (
+        <section className="pt-5 pb-14">
+            <div className="container" ref={containerRef}>
+                <TransitionLink
+                    back
+                    href="/"
+                    className="inline-flex gap-2 items-center group h-12"
+                >
+                    <ChevronLeft
+                        size={35}
+                        strokeWidth={1.25}
+                        className="group-hover:-translate-x-1 group-hover:text-primary transition-all duration-300"
+                    />
+                    Back
+                </TransitionLink>
+
+                <div
+                    className="top-0 min-h-[calc(100svh-100px)] flex"
+                    id="info"
+                >
+                    <div className="relative w-full">
+                        <div className="flex items-start gap-6 mx-auto mb-10 max-w-[635px]">
+                            <h1 className="fade-in-later opacity-0 text-4xl md:text-[60px] leading-none font-anton overflow-hidden">
+                                <span className="inline-block">{id}</span>
+                            </h1>
+
+                            <div className="fade-in-later opacity-0 flex gap-2"></div>
+                        </div>
+
+                        <div className="max-w-[635px] space-y-7 pb-20 mx-auto">
+                            <div className="fade-in-later">
+                                <p className="text-muted-foreground font-anton mb-3">
+                                    Year
+                                </p>
+                            </div>
+                            <div className="fade-in-later">
+                                <p className="text-muted-foreground font-anton mb-3">
+                                    Tech & Technique
+                                </p>
+
+                                <div className="text-lg"></div>
+                            </div>
+                            <div className="fade-in-later">
+                                <p className="text-muted-foreground font-anton mb-3">
+                                    Description
+                                </p>
+
+                                <div className="text-lg prose-xl markdown-text"></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div
+                    className="fade-in-later relative flex flex-col gap-2 max-w-[800px] mx-auto"
+                    id="images"
+                ></div>
+            </div>
+        </section>
+    )
 }
