a3f8467f860cfffa167913f6c4a7a84a38d8799c
code-wave <codewave080@gmail.com>
2025-05-31 00:01:03 +0900

    feat: MyExperience 섹션에 GSAP 애니메이션 추가
    스크롤 기반 애니메이션을 위해 GSAP와 ScrollTrigger를 도입하여 사용자 참여도를 향상시켰습니다. 특정 스크롤 위치에서 트리거되는 경험 항목들과 컨테이너의 동작을 위한 애니메이션을 추가했습니다. 애니메이션 타겟팅을 위해 컨테이너에 ref를 사용하도록 업데이트했습니다.

--- src/app/components/MyExperience.tsx
+++ src/app/components/MyExperience.tsx
@@ -1,9 +1,56 @@
-import React from 'react'
+import React, {useRef} from 'react'
+import { useGSAP } from '@gsap/react';
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/all';
+
+gsap.registerPlugin(useGSAP, ScrollTrigger);
 
 export default function MyExperience() {
+    const containerRef = useRef<HTMLDivElement>(null);
+
+    useGSAP(
+        () => {
+            const tl = gsap.timeline({
+                scrollTrigger: {
+                    trigger: containerRef.current,
+                    start: 'top 60%',
+                    end: 'bottom 50%',
+                    toggleActions: 'restart none none reverse',
+                    scrub: 1,
+                },
+            });
+
+            tl.from('.experience-item', {
+                y: 50,
+                opacity: 0,
+                stagger: 0.3,
+            });
+        },
+        { scope: containerRef },
+    );
+
+    useGSAP(
+        () => {
+            const tl = gsap.timeline({
+                scrollTrigger: {
+                    trigger: containerRef.current,
+                    start: 'bottom 50%',
+                    end: 'bottom 20%',
+                    scrub: 1,
+                },
+            });
+
+            tl.to(containerRef.current, {
+                y: -150,
+                opacity: 0,
+            });
+        },
+        { scope: containerRef },
+    );
+
     return (
         <section className="pb-section text-[#DEDEDE]" id="my-experience">
-            <div className="container">
+            <div ref={containerRef} className="container">
                 <div className="flex items-center gap-4 mb-10 text-[#DEDEDE]">
                     <h2 className="text-xl uppercase leading-none">
                         MY EXPERIENCE
