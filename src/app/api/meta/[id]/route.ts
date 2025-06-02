6fcbc3aa266e5637c1cfd7360ca4498c22ba62df
code-wave <codewave080@gmail.com>
2025-06-02 22:35:07 +0900

    feat: CARD 프로젝트 이미지 추가 및 WEDDING, TWITTER 프로젝트 id 수정

--- src/app/api/meta/[id]/route.ts
+++ src/app/api/meta/[id]/route.ts
@@ -26,9 +26,45 @@ const metadataList = [
                 <li>컴포넌트 구조 개선: 로직 분리 및 합성 패턴 적용으로 재사용성과 유지보수성 향상</li>
             </ul>
         `,
+        images: [
+            'mcard_1',
+            'mcard_2',
+            'mcard_3',
+            'mcard_4',
+            'mcard_5',
+            'mcard_6',
+        ],
     },
     {
         id: '2',
+        title: 'WEDDING',
+        description:
+            '결혼식 정보를 효과적으로 전달하면서도 웹 성능과 사용자 경험을 함께 고려한 최적화 중심의 디지털 청첩장 웹 애플리케이션입니다. 인트로 영상, 이미지 갤러리, 참석 여부 확인, 지도, 연락처, 공유 기능 등 실사용을 염두에 둔 기능을 직접 구현했으며 미디어 자산 최적화와 상태 관리 전략을 적용해 빠르고 안정적인 사용자 경험을 제공하는 데 집중했습니다.',
+        year: '2025',
+        url: 'https://example.com/3',
+        techStacks: ['React', 'typescript', 'SASS', 'Firebase', 'Vercel'],
+        mainFeatures: `
+            <ul class="list-disc pl-6">
+                <li>인트로 영상 & 갤러리: WebM 기반 경량 영상, Swiper를 활용한 이미지 갤러리 구현</li>
+                <li>청첩장 본문 / 지도 / 일정 안내: KakaoMap, Day Picker를 활용한 위치 및 날짜 표시</li>
+                <li>연락처 섹션: 계좌 복사, 전화 연결, 카카오페이 송금 기능을 아코디언 UI로 구성</li>
+                <li>참석 여부 확인: 모달 기반 입력, 로컬 스토리지로 상태 저장 처리</li>
+                <li>공유 기능: 카카오톡 공유 및 URL 복사 기능 제공</li>
+            </ul>
+            `,
+        technicalImprovements: `
+            <ul class="list-disc pl-6">
+                <li>미디어 최적화: Squoosh 압축, WebM 포맷 변환, CDN 적용으로 이미지 및 영상 로딩 개선</li>
+                <li>렌더링 성능 최적화: useMemo, useCallback, Suspense를 활용한 불필요한 렌더링 방지</li>
+                <li>안정성 확보: ErrorBoundary를 통한 하위 컴포넌트 오류 격리 및 Fallback UI 제공</li>
+                <li>구조 개선: 로직과 뷰를 분리하여 재사용성과 유지보수성이 높은 컴포넌트 구조 설계</li>
+                <li>UX 향상: 명확한 진행 피드백, 정보 공유 편의성, 다양한 사용자 환경 대응 고려</li>
+            </ul>
+        `,
+        images: []
+    },
+    {
+        id: '3',
         title: 'TWITTER',
         description:
             '실시간 타임라인, OAuth 로그인, 해시태그 검색 등 SNS 서비스의 핵심 기능을 React와 Firebase 기반으로 구현한 프로젝트입니다. Firestore 실시간 동기화, 이미지 업로드, 사용자 인증, 다국어 처리 등 다양한 기능을 통해 사용자 중심의 흐름과 상태 관리 구조를 직접 설계했으며 모바일 대응 및 전역 상태 분리를 고려한 확장 가능한 구조로 구성했습니다.',
@@ -61,33 +97,6 @@ const metadataList = [
                 <li>라우팅 흐름 최적화: 인증 여부에 따라 접근 제어하는 AuthGuard 유사 구조 구현</li>
                 <li>다국어 처리: 언어 설정 전역화 및 즉시 반영 가능한 전환 구조 구현</li>
                 <li>모바일 대응 설계: 미디어 쿼리 기반 UI 대응 및 유동적 레이아웃 구성</li>
-            </ul>
-        `,
-    },
-    {
-        id: '3',
-        title: 'WEDDING',
-        description:
-            '결혼식 정보를 효과적으로 전달하면서도 웹 성능과 사용자 경험을 함께 고려한 최적화 중심의 디지털 청첩장 웹 애플리케이션입니다. 인트로 영상, 이미지 갤러리, 참석 여부 확인, 지도, 연락처, 공유 기능 등 실사용을 염두에 둔 기능을 직접 구현했으며 미디어 자산 최적화와 상태 관리 전략을 적용해 빠르고 안정적인 사용자 경험을 제공하는 데 집중했습니다.',
-        year: '2025',
-        url: 'https://example.com/3',
-        techStacks: ['React', 'typescript', 'SASS', 'Firebase', 'Vercel'],
-        mainFeatures: `
-            <ul class="list-disc pl-6">
-                <li>인트로 영상 & 갤러리: WebM 기반 경량 영상, Swiper를 활용한 이미지 갤러리 구현</li>
-                <li>청첩장 본문 / 지도 / 일정 안내: KakaoMap, Day Picker를 활용한 위치 및 날짜 표시</li>
-                <li>연락처 섹션: 계좌 복사, 전화 연결, 카카오페이 송금 기능을 아코디언 UI로 구성</li>
-                <li>참석 여부 확인: 모달 기반 입력, 로컬 스토리지로 상태 저장 처리</li>
-                <li>공유 기능: 카카오톡 공유 및 URL 복사 기능 제공</li>
-            </ul>
-            `,
-        technicalImprovements: `
-            <ul class="list-disc pl-6">
-                <li>미디어 최적화: Squoosh 압축, WebM 포맷 변환, CDN 적용으로 이미지 및 영상 로딩 개선</li>
-                <li>렌더링 성능 최적화: useMemo, useCallback, Suspense를 활용한 불필요한 렌더링 방지</li>
-                <li>안정성 확보: ErrorBoundary를 통한 하위 컴포넌트 오류 격리 및 Fallback UI 제공</li>
-                <li>구조 개선: 로직과 뷰를 분리하여 재사용성과 유지보수성이 높은 컴포넌트 구조 설계</li>
-                <li>UX 향상: 명확한 진행 피드백, 정보 공유 편의성, 다양한 사용자 환경 대응 고려</li>
             </ul>
         `,
     },
