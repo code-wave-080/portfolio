c36587e83318da1cbce8bf0fe0a9f493ac7e3a34
code-wave <codewave080@gmail.com>
2025-06-11 00:12:15 +0900

    chore: 미사용 Experience 페이지 파일 삭제

---
+++ src/app/career/[id]/page.tsx
@@ -0,0 +1,64 @@
+'use client'
+
+import { use, useEffect, useRef, useState } from 'react'
+import TransitionLink from '@/components/TransitionLink'
+import { ChevronLeft } from 'lucide-react'
+import { CareerItem } from '@/app/components/MyExperience'
+
+interface CareerPageProps {
+    id: string
+}
+
+export default function CareerPage({
+    params,
+}: {
+    params: Promise<CareerPageProps>
+}) {
+    const { id } = use(params) as CareerPageProps
+    const containerRef = useRef<HTMLDivElement>(null)
+    const [career, setCareer] = useState<CareerItem | null>(null)
+
+    useEffect(() => {
+        const fetchProject = async () => {
+            const res = await fetch(`/api/meta/${id}`)
+            const data = await res.json()
+            setCareer(data)
+        }
+
+        fetchProject().catch(console.error)
+    }, [id])
+
+    return (
+        <section className="pt-5 pb-14">
+            <div className="container" ref={containerRef}>
+                <TransitionLink
+                    back
+                    href="/"
+                    className="inline-flex gap-2 items-center group h-12"
+                >
+                    <ChevronLeft
+                        size={35}
+                        strokeWidth={1.25}
+                        className="group-hover:-translate-x-1 group-hover:text-primary transition-all duration-300"
+                    />
+                    Back
+                </TransitionLink>
+
+                <div
+                    className="top-0 min-h-[calc(100svh-100px)] flex mt-16"
+                    id="info"
+                >
+                    <div className="relative w-full">
+                        <div className="flex items-start gap-6 mx-auto mb-10 max-w-[635px]">
+                            <h1 className="fade-in-later text-4xl md:text-[60px] leading-none overflow-hidden">
+                                <span className="inline-block text-[#DEDEDE]">
+                                    {career?.companyName}
+                                </span>
+                            </h1>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
