89eb1ccc3fea1dcadbf0e9baa7670d61e20a102e
code-wave <codewave080@gmail.com>
2025-06-12 00:15:21 +0900

    refactor: 프로젝트 페이지 console.log 제거

--- src/app/projects/[id]/page.tsx
+++ src/app/projects/[id]/page.tsx
@@ -42,8 +42,6 @@ export default function ProjectPage({
         fetchProject().catch(console.error)
     }, [id])
 
-    console.log(project)
-
     useGSAP(
         () => {
             if (!containerRef.current) {
