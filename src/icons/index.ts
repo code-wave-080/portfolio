12eb7e74981b1a986fe4e6b865de8861d8b10764
code-wave <codewave080@gmail.com>
2025-07-01 18:22:24 +0900

    feat: 아이콘 import 효율화 위해 barrel 파일 index.ts 도입

---
+++ src/icons/index.ts
@@ -0,0 +1,49 @@
+import JavascriptIcon from './javascript.svg'
+import TypescriptIcon from './typescript.svg'
+import VueIcon from './vuedotjs.svg'
+import ReactIcon from './react.svg'
+import NextIcon from './nextdotjs.svg'
+import TailwindIcon from './tailwindcss.svg'
+import FramerIcon from './framer.svg'
+import SassIcon from './sass.svg'
+import StyledComponentsIcon from './styledcomponents.svg'
+import GitIcon from './git.svg'
+import GithubIcon from './github.svg'
+import VercelIcon from './vercel.svg'
+import FirebaseIcon from './firebase.svg'
+import EslintIcon from './eslint.svg'
+import PrettierIcon from './prettier.svg'
+import StorybookIcon from './storybook.svg'
+import IntellijIcon from './intellijidea.svg'
+import EchartsIcon from './apacheecharts.svg'
+import SlackIcon from './slack.svg'
+import NotionIcon from './notion.svg'
+import FigmaIcon from './figma.svg'
+import GiraIcon from './jira.svg'
+import EmotionIcon from './emotion.png'
+
+export {
+    JavascriptIcon,
+    TypescriptIcon,
+    VueIcon,
+    ReactIcon,
+    NextIcon,
+    TailwindIcon,
+    FramerIcon,
+    SassIcon,
+    StyledComponentsIcon,
+    GitIcon,
+    GithubIcon,
+    VercelIcon,
+    FirebaseIcon,
+    EslintIcon,
+    PrettierIcon,
+    StorybookIcon,
+    IntellijIcon,
+    EchartsIcon,
+    SlackIcon,
+    NotionIcon,
+    FigmaIcon,
+    GiraIcon,
+    EmotionIcon,
+}
