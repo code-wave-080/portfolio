aebceaf2cc53c8805df3c71e7d7dd991b71549ce
code-wave <codewave080@gmail.com>
2025-07-15 23:56:57 +0900

    refactor: BalloonText 컴포넌트 스타일 조정 및 조명 설정 개선

--- src/app/components/ThreeText.tsx
+++ src/app/components/ThreeText.tsx
@@ -1,38 +1,50 @@
 'use client'
 
-import React, { useRef } from 'react'
-import { Canvas, useFrame } from '@react-three/fiber'
-import { OrbitControls, Text3D, Center } from '@react-three/drei'
+import React from 'react'
+import { Canvas } from '@react-three/fiber'
+import { Center, Text3D } from '@react-three/drei'
 
-export default function ThreeText() {
+export default function BalloonText() {
     return (
-        <Canvas
-            style={{
-                width: '100vw',
-                height: '200px',
-                backgroundColor: 'transparent'
-            }}
-            gl={{ alpha: true }} // 투명 배경 설정
-        >
-            <ambientLight intensity={1} />
-            <directionalLight position={[2, 2, 2]} />
-            <Center>
-                <Text3D
-                    font="/fonts/helvetiker_regular.typeface.json"
-                    size={2}
-                    height={0.2}
-                    curveSegments={12}
-                    bevelEnabled
-                    bevelThickness={0.03}
-                    bevelSize={0.02}
-                    bevelOffset={0}
-                    bevelSegments={5}
-                >
-                    Frontend
-                    <meshStandardMaterial color="#00adef" />
-                </Text3D>
-            </Center>
-            <OrbitControls enableZoom={false} />
-        </Canvas>
+        <div style={{ width: '100vw', height: '200px', background: '#fceeff' }}>
+            <Canvas
+                orthographic
+                camera={{ zoom: 70, position: [0, 0, 100] }}
+                gl={{ alpha: true }}
+            >
+                <ambientLight intensity={0.3} />
+                <pointLight position={[10, 10, 10]} intensity={2} />
+                <spotLight
+                    position={[-10, 10, 10]}
+                    intensity={1.5}
+                    angle={0.3}
+                />
+
+                <Center>
+                    <Text3D
+                        font="/fonts/helvetiker_regular.typeface.json" // 풍선 느낌 폰트 사용
+                        size={1.2}
+                        height={0.5}
+                        curveSegments={20}
+                        bevelEnabled
+                        bevelThickness={0.2}
+                        bevelSize={0.1}
+                        bevelOffset={0}
+                        bevelSegments={10}
+                    >
+                        Frontend
+                        <meshPhysicalMaterial
+                            color="#8f6eff"
+                            roughness={0.1}
+                            metalness={0.5}
+                            clearcoat={1}
+                            clearcoatRoughness={0.1}
+                            emissive="#cabfff"
+                            emissiveIntensity={0.2}
+                        />
+                    </Text3D>
+                </Center>
+            </Canvas>
+        </div>
     )
 }
