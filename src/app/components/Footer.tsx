0c14814b71efb2771a789505c14073da11cf70ef
code-wave <codewave080@gmail.com>
2025-08-05 22:04:21 +0900

    refactor: Footer 문구 수정

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -3,9 +3,8 @@ export default function Footer() {
         <footer className="text-center pb-5 text-[#DEDEDE]">
             <div className="container">
                 <p className="text-lg">
-                    프로젝트 협업 또는 채용 문의를 기다리고 있습니다
+                    오늘보다 나은 개발을 위해, 내일도 배우고 고민합니다.
                 </p>
-                <p className="text-lg">언제든지 메일로 연락 주세요</p>
                 <a
                     href="mailto:codewave080@gmail.com"
                     className="text-3xl sm:text-4xl font-anton font-bold inline-block mt-5 mb-10 hover:underline"
